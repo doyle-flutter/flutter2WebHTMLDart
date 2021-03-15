86a8cd6775302eaa2a35d1379dab1fb8ebeced48
doyle <il4479@hanmail.net>
2021-03-15 20:38:41 +0900

    read

---
+++ android/app/src/main/kotlin/com/doyle/flutter2html/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.doyle.flutter2html
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'dart:html' as html;
+void main() => runApp(MaterialApp(home: WebPage(),));
+
+class WebPage extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
